de3c857432a9f0e069d1cfcd39fb8520304989ff
Aline Ponzani <alinef.ponzani@gmail.com>
2023-05-29 21:14:54 -0300

    enviar

--- src/Pages/CadastroMorador/index.js
+++ src/Pages/CadastroMorador/index.js
@@ -47,30 +47,32 @@ function CadastroMorador() {
         }
     }
 
-    const enviarDados = async (event) => {
-        event.preventDefault();
-    
-        try {
-          const response = await fetch('http://localhost:3333/cadastroUsuario', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formDados),
-          });
-    
-          const data = await response.json();
-          if (data.success) {
-            // Login válido
-            console.log(data.user.nome);
-          } else {
-            // Login inválido
-            alert("Erro de teste");
-          }
-        } catch (error) {
-          console.error('Ocorreu um erro ao fazer login:', error);
+
+
+    async function enviarDados(event) {
+        if (formDados.email === "") {
+            alert("email vazio")
+        } else {
+            alert(formDados.email)
         }
-      };
+        event.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3333/cadastroUsuario', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formDados)
+            });
+            const aux = response.json();
+            console.log(response);
+            console.log(aux);
+
+        } catch {
+            console.error('Ocorreu um erro ao fazer login:');
+        }
+    };
 
 
 
@@ -80,21 +82,21 @@ function CadastroMorador() {
             <div className="cont">
                 <h1>CADASTRO DE MORADOR</h1>
                 <form onSubmit={enviarDados}>
-                    <input id="nome" type="text" placeholder="Nome" value={formDados.nome} onChange={(e) => setDados((prevDados) => ({ ...prevDados, nome: e.target.value }))} required/>
-                    <input id="telefone" type="tel" placeholder="Telefone" value={formDados.telefone} onChange={(e) => setDados((prevDados) => ({ ...prevDados, telefone: e.target.value }))} required/>
-                    <input id="email" type="email" placeholder="E-mail" value={formDados.email} onChange={(e) => setDados((prevDados) => ({ ...prevDados, email: e.target.value }))} required/>
-                    <input id="dataNac" type="date" placeholder="Data de Nacsimento" value={formDados.dataNac} onChange={(e) => setDados((prevDados) => ({ ...prevDados, dataNac: e.target.value }))} required/>
-                    <input id="cep" type="text" maxlength="8" placeholder="Cep" onChange={(e) => setDados((prevDados) => ({ ...prevDados, cep: e.target.value }))} onBlur={buscarCep} required/>
-                    <input id="cidade" type="text" placeholder="Cidade" value={formDados.cidade} onChange={(e) => setDados((prevDados) => ({ ...prevDados, cidade: e.target.value }))} required/>
-                    <input id="bairro" type="text" placeholder="Bairro" value={formDados.bairro} onChange={(e) => setDados((prevDados) => ({ ...prevDados, bairro: e.target.value }))} required/>
-                    <input id="logradouro" type="text" placeholder="Logradouro" value={formDados.logradouro} onChange={(e) => setDados((prevDados) => ({ ...prevDados, logradouro: e.target.value }))} required/>
-                    <input id="estado" type="text" placeholder="Estado" value={formDados.estado} onChange={(e) => setDados((prevDados) => ({ ...prevDados, estado: e.target.value }))} required/>
-                    <input id="numero" type="number" placeholder="Número" value={formDados.numero} onChange={(e) => setDados((prevDados) => ({ ...prevDados, numero: e.target.value }))} required/>
-                    <input id="blocoTorre" type="text" placeholder="Bloco/Torre" value={formDados.blocoTorre} onChange={(e) => setDados((prevDados) => ({ ...prevDados, blocoTorre: e.target.value }))} required/>
-                    <input id="apartamento" type="number" placeholder="Apartamento" value={formDados.apartamento} onChange={(e) => setDados((prevDados) => ({ ...prevDados, apartamento: e.target.value }))} required/>
-                    <input id="nomeCondominio" type="text" placeholder="Qual o nome do seu condomínio" value={formDados.nomeCondominio} onChange={(e) => setDados((prevDados) => ({ ...prevDados, nomeCondominio: e.target.value }))} required/>
-                    <input id="senha" type="password" placeholder="Senha" value={formDados.senha} onChange={(e) => setDados((prevDados) => ({ ...prevDados, senha: e.target.value }))} required/>
-                    <button type="submit">Salvar</button> 
+                    <input id="nome" type="text" placeholder="Nome" value={formDados.nome} onChange={(e) => setDados({ ...formDados, nome: e.target.value })} />
+                    <input id="telefone" type="text" placeholder="Telefone" value={formDados.telefone} onChange={(e) => setDados({ ...formDados, telefone: e.target.value })} />
+                    <input id="email" type="text" placeholder="E-mail" value={formDados.email} onChange={(e) => setDados({ ...formDados, email: e.target.value })} />
+                    <input id="dataNac" type="text" placeholder="Data de Nacsimento" value={formDados.dataNac} onChange={(e) => setDados({ ...formDados, dataNac: e.target.value })} />
+                    <input id="cep" type="text" maxLength="8" placeholder="Cep" onChange={(e) => setDados({ ...formDados, cep: e.target.value })} onBlur={buscarCep} />
+                    <input id="cidade" type="text" placeholder="Cidade" value={formDados.cidade} onChange={(e) => setDados({ ...formDados, cidade: e.target.value })} />
+                    <input id="bairro" type="text" placeholder="Bairro" value={formDados.bairro} onChange={(e) => setDados({ ...formDados, bairro: e.target.value })} />
+                    <input id="logradouro" type="text" placeholder="Logradouro" value={formDados.logradouro} onChange={(e) => setDados({ ...formDados, logradouro: e.target.value })} />
+                    <input id="estado" type="text" placeholder="Estado" value={formDados.estado} onChange={(e) => setDados({ ...formDados, estado: e.target.value })} />
+                    <input id="numero" type="number" placeholder="Número" value={formDados.numero} onChange={(e) => setDados({ ...formDados, numero: e.target.value })} />
+                    <input id="blocoTorre" type="number" placeholder="Bloco/Torre" value={formDados.blocoTorre} onChange={(e) => setDados({ ...formDados, blocoTorre: e.target.value })} />
+                    <input id="apartamento" type="number" placeholder="Apartamento" value={formDados.apartamento} onChange={(e) => setDados({ ...formDados, apartamento: e.target.value })} />
+                    <input id="nomeCondominio" type="text" placeholder="Qual o nome do seu condomínio" value={formDados.nomeCondominio} onChange={(e) => setDados({ ...formDados, nomeCondominio: e.target.value })} />
+                    <input id="senha" type="text" placeholder="Senha" />
+                    <button type="submit">Salvar</button>
                 </form>
             </div>
         </div>
